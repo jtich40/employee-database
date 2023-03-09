630f414a3c0e792b5b678f8ce0f4b5c786c7c8e1
Jared Tichacek <jared.tichacek@gmail.com>
2023-03-08 21:28:46 -0600

    started addEmployee function, need to fix prompt for manager selection to isolate managers only

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function menu() {
                     break;
 
                 case 'Add an employee':
-                    // call view all departments
+                    addEmployee()
                     break;
 
                 case 'Update an employee role':
@@ -126,7 +126,7 @@ function viewRoles() {
   function addRole() {
     db.promise().query('SELECT * FROM department') 
     .then(([data]) => {
-        const deptChoices = data.map(({ id, name }) => ({ name: name, id: id}))
+        const deptChoices = data.map(({ id, name }) => ({ name: name, value: id}))
         inquirer
             .prompt([
                 {
@@ -157,4 +157,111 @@ function viewRoles() {
     })
         
 }
+
+function addRole() {
+    db.promise().query('SELECT * FROM department') 
+    .then(([data]) => {
+        const deptChoices = data.map(({ id, name }) => ({ 
+            name: name, 
+            value: id
+        }))
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: "What is the name of the role?",
+                },
+    
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: "What is the salary of the role?",
+                },
+    
+                {
+                    type: 'list',
+                    name: 'department',
+                    message: "What department will this role be added to?",
+                    choices: deptChoices
+                }
+            ])
+            .then(res => {
+                db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',  [res.title, res.salary, res.department], (error, data) => {
+                    console.log('Role successfully added!')
+                    menu()
+                })
+            })
+    })
+        
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: "What is the first name of the new employee?",
+            },
+
+            {
+                type: 'input',
+                name: 'last_name',
+                message: "What is the last name of the new employee?",
+            }
+        ])
+        .then(res => {
+            const firstName = res.first_name
+            const LastName = res.last_name
+
+            db.promise().query('SELECT * FROM role')
+                .then(([data]) => {
+                    const roleChoices = data.map(({ id, title }) => ({
+                        name: title,
+                        value: id
+                    }))
+                    inquirer
+                        .prompt([
+                            {
+                                type: 'list',
+                                name: 'role',
+                                message: "What role will be filled by the new employee?",
+                                choices: roleChoices
+                            }
+                        ])
+                        .then(res => {
+                            const role = res.role
+
+                            db.promise().query('SELECT * FROM employee')
+                                .then(([data]) => {
+                                    const managerChoices = data.map(({ first_name, last_name, manager_id }) => ({
+                                        name: `${first_name} ${last_name}`,
+                                        value: manager_id
+                                    }))
+
+                                    managerChoices.push({ name: "None", value: null });
+
+                                    inquirer
+                                        .prompt([
+                                            {
+                                                type: 'list',
+                                                name: 'manager',
+                                                message: "What manager will this role be reporting to?",
+                                                choices: managerChoices
+                                            }
+                                        ])
+
+                                        .then(res => {
+                                            db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [firstName, LastName, role, res.manager_id], (error, data) => {
+                                                console.log('Employee successfully added!')
+                                                menu()
+                                            })
+                                        })
+                                })
+                        })
+                })
+        })
+}
+
 
